9fc6e58c78f191992ac43c8a82e9e202acb66a78
Colby Norman <colbynorm41@gmail.com>
2020-01-30 22:20:13 -0600

    added files

--- calendar.js
+++ calendar.js
@@ -1,36 +1,37 @@
-function clock() {
+$(document).ready(function() {
+  function clock() {
     var now = moment().format("dddd, MMMM Do YYYY, hh:mm:ss A");
     $("#currentDay").html(now);
     setTimeout(clock, 1000);
-}
-var hour = moment().format("HH")
-hour = parseInt(hour);
-console.log(typeof hour);
-console.log(hour)
+  }
+  var hour = moment().format("HH");
+  hour = parseInt(hour);
+  //console.log(typeof hour);
+  //console.log(hour)
 
-clock();
-
-var saveButton = $(".saveButton");
-
-saveButton.on("click", function(event){
-    event.target
-    var task = $(this).siblings("textarea").val();
+  var saveButton = $(".saveButton");
+  console.log(saveButton);
+  saveButton.on("click", function() {
+    console.log("click");
+    var task = $(this)
+      .siblings("textarea")
+      .val();
     var time = $(this).attr("id");
     localStorage.setItem(time, task);
-})
+  });
 
-var calendarHour = $("#time");
-var currentHour = moment().hours();
-$("textarea").each(function(){
-    if(calendarHour > currentHour){
-    $(this).addClass("future");
+  var currentHour = moment().hours();
+  $("textarea").each(function() {
+    var calendarHour = $(this).attr("id");
+    //console.log($(this).attr("id"));
+    if (calendarHour > currentHour) {
+      $(this).addClass("future");
+    } else if (calendarHour < currentHour) {
+      $(this).addClass("past");
+    } else if (calendarHour === currentHour) {
+      $(this).addClass("present");
     }
-    else if(calendarHour < currentHour){
-    $(this).addClass("past");
-    }
-    else if(calendarHour === currentHour){
-    $(this).addClass("present");
-    }
-})
-
-//.text() .val() .show()/.hide() .append() .empty() .siblings() .attr() .css() .html()
+  });
+  clock();
+  //.text() .val() .show()/.hide() .append() .empty() .siblings() .attr() .css() .html()
+});
